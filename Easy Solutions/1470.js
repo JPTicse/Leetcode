02d570918188c0e0aec9ba0af29146fef3f5700b
JPTicse <jpticse@gmail.com>
2024-03-08 20:02:47 -0500

    LeetCode 1470 solved

---
+++ Easy Solutions/1470.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    ans = []
+    for (let i=0; i<nums.length/2; i++){
+        ans.push(nums[i], nums[i+n]);
+    }
+    return ans;
+};
